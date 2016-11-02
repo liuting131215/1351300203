22845a82cc81b1f9ce056d6383042e487dc00cef
stu <stu@qq.com>
2016-11-02 12:33:06 +0800

    11

---
+++ News/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        /// <summary>
+        /// Welcome页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新闻列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List(int page=3)
+        {
+            string[] data = new string[]
+            {
+                "头条型新闻一",
+                "爆炸型新闻二",
+                "小众型新闻三"
+            };
+            ViewBag.data = data;
+            ViewBag.page = page;
+           // ViewData.Model = data;
+            return View();
+        }
+        /// <summary>
+        /// 添加新闻
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Add()
+        {
+           
+
+            return View();
+        }
+        /// <summary>
+        /// 保存新闻内容
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Save(string title,string content)
+        {
+            ViewBag.Title = title;
+            ViewBag.Content = content;
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
